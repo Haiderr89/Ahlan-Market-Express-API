1447d57027121549e86efe09a7c4197b913475d9
Sayed Haider <sayedhaider2002@gmail.com>
2025-03-02 15:33:40 +0300

    done with the controller

--- controllers/market.js
+++ controllers/market.js
@@ -88,15 +88,15 @@ router.post('/', async (req, res) => {
 
   //comments ==================
 
-router.post('/:hootId/comments', async (req, res) => {
+router.post('/:marketId/comments', async (req, res) => {
     try {
       req.body.author = req.user._id;
-      const hoot = await Hoot.findById(req.params.hootId);
-      hoot.comments.push(req.body);
-      await hoot.save();
+      const market = await marketPlace.findById(req.params.marketId);
+      market.comments.push(req.body);
+      await market.save();
   
       // Find the newly created comment:
-      const newComment = hoot.comments[hoot.comments.length - 1];
+      const newComment = market.comments[market.comments.length - 1];
   
       newComment._doc.author = req.user;
   
@@ -107,23 +107,23 @@ router.post('/:hootId/comments', async (req, res) => {
     }
   });
 
-  router.put('/:hootId/comments/:commentId', async (req, res) => {
+  router.put('/:marketId/comments/:commentId', async (req, res) => {
     try {
-      const hoot = await Hoot.findById(req.params.hootId);
-      const comment = hoot.comments.id(req.params.commentId);
+      const market = await marketPlace.findById(req.params.marketId);
+      const comment = market.comments.id(req.params.commentId);
       comment.text = req.body.text;
-      await hoot.save();
+      await market.save();
       res.status(200).json({ message: 'Ok' });
     } catch (err) {
       res.status(500).json(err);
     }
   });
 
-  router.delete('/:hootId/comments/:commentId', async (req, res) => {
+  router.delete('/:marketId/comments/:commentId', async (req, res) => {
     try {
-      const hoot = await Hoot.findById(req.params.hootId);
-      hoot.comments.remove({ _id: req.params.commentId });
-      await hoot.save();
+      const market = await marketPlace.findById(req.params.marketId);
+      market.comments.remove({ _id: req.params.commentId });
+      await market.save();
       res.status(200).json({ message: 'Ok' });
     } catch (err) {
       res.status(500).json(err);
